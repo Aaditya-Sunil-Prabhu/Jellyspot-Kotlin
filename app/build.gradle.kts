0c7086408704fed649b09fc1da940f967ebc11c7
Aaditya Prabhu <aaditya.prabhu@gmail.com>
2025-12-25 23:49:33 +0530

    Upgrade Hilt to 2.53.1 for Kotlin 2.1 support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,8 +85,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.5")
     
     // Hilt Dependency Injection
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    ksp("com.google.dagger:hilt-compiler:2.51.1")
+    implementation("com.google.dagger:hilt-android:2.53.1")
+    ksp("com.google.dagger:hilt-compiler:2.53.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     
     // Media3 (ExoPlayer)
